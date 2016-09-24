9c812c47ba0812b0bf92d8bb9cea94d4b72c092c
liufei <liufei@chupinxiu.com>
2016-09-24 10:43:58 +0800

    table

--- app/scripts/controllers/IndexCtrl.js
+++ app/scripts/controllers/IndexCtrl.js
@@ -8,6 +8,9 @@ define(['adminApp'], function(adminApp) {
 		HttpGet.httpgetlist('../../json/index.json',function(data){
 			$scope.welcomeText = data;
 		});
+		HttpGet.httpgetlist('../../json/userlist.json',function(data){
+			$scope.userList = data;
+		});
 	}])
 	.controller('datepickerCtrl', function ($scope) {
 	    $scope.datePicker = {startDate: '2015-4-5', endDate: '2015-5-6'};
